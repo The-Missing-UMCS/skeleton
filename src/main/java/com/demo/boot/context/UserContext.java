389f59915270c1bc4fd54bd15ecce4eed4090852
fyiernzy <fyiernzy@gmail.com>
2025-10-11 09:00:59 +0800

    feat: Add sample code for UserContext

--- src/main/java/com/demo/boot/context/UserContext.java
+++ src/main/java/com/demo/boot/context/UserContext.java
@@ -1,5 +1,13 @@
 package com.demo.boot.context;
 
+import lombok.Data;
+
+@Data
 public class UserContext {
 
+    private static String username;
+
+    public static void setUsername(String username) {
+        UserContext.username = username;
+    }
 }
